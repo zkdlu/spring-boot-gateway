dd7cb23077e7cdab65003205d66ff0bf181d4104
zkdlu <zkdlu159951@gmail.com>
2020-12-12 15:48:08 +0900

    simple gateway

---
+++ servicevb/src/main/java/com/zkdlu/servicevb/ServicevbApplication.java
@@ -0,0 +1,13 @@
+package com.zkdlu.servicevb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicevbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicevbApplication.class, args);
+    }
+
+}
